2876a2cb7fbf3008495559bed795426412aa8098
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-06-15 19:37:29 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatchspring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchspringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchspringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
